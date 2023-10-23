49e4a20d512d5f7b8fe89ff7296a5cb65d907b69
deimerhdz <deimerhdz21@gmail.com>
2023-10-23 11:14:43 -0500

    validate model items in purchase

--- validators/purchase.validator.js
+++ validators/purchase.validator.js
@@ -5,6 +5,9 @@ const { validateFields } = require("../helpers/validate-field");
 const createPurchaseValidator = [
     check('total','storeId is required').not().isEmpty(),
     check('items','items is required').not().isEmpty(),
+    check('items.*.productId','productId is required').not().isEmpty(),
+    check('items.*.quantity','quantity is required').not().isEmpty(),
+    check('items.*.unitCost','unitCost is required').not().isEmpty(),
     check('items','items is not valid').isArray({min:1}),
     check('storeId','storeId is required').not().isEmpty(),
     check('storeId').custom(existStoreById),
